bf12f919b37e29bc9e2b905e0e3e9ad4de92f9c0
adi0229 <jiandi.huang@gmail.com>
2019-06-17 21:33:21 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://www.googleapis.com/drive/v3/files/1a-lYab6qIWmgpt1QYEUWvr6jx3CY2Ypl?alt=media&key=AIzaSyCjfBNpIjtoI5HshPtKyx-05DYphh1dpvE'
 export_file_name = 'export.pkl'
 
 classes = ['coca','other','peisi']
